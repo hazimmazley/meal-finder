5fa98759edc8e0ed6bfa445aad549affca1a2f03
Hazim Mazley <hazimmazley@gmail.com>
2022-07-17 16:47:09 +0800

    get random meal

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function searchMeal(e) {
     }
 }
 
-// Featch meal by Id
+// Fetch meal by Id
 function getMealById(mealID) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
     .then(res => res.json())
@@ -51,6 +51,20 @@ function getMealById(mealID) {
       const meal = data.meals[0];
 
       addMealToDOM(meal);
+    });
+}
+
+// Fetch random meal from API
+function getRandomMeal() {
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then(res => res.json())
+    .then(data => {
+        const meal = data.meals[0];
+
+        addMealToDOM(meal);
     });
 }
 
@@ -85,6 +99,7 @@ function addMealToDOM(meal) {
 }
 
 submit.addEventListener('submit', searchMeal)
+random.addEventListener('click', getRandomMeal)
 
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
